e16d84e4314d8de0d4de7405f70b623e88fd9a3d
Suryansh Kushwaha <suryanshkushwaha1235@gmail.com>
2024-12-08 22:13:42 +0530

    Frontend setup with Shadcn.ui

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-pink-400">
+      Hello world!
+    </h1>
+    <Button>Hello JI! </Button>
+    </>
+  )
+}
